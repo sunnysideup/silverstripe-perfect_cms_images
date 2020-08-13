b562029379a96145002e25befa2655bfe69afef8
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-08-13 12:11:07 +1200

    MAJOR: revamp from 3

---
+++ src/Api/ImageManipulations.php
@@ -0,0 +1,241 @@
+<?php
+
+namespace Sunnysideup\PerfectCmsImages\Api;
+
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\Folder;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\ORM\FieldType\DBField;
+use SilverStripe\ORM\SS_List;
+use Sunnysideup\PerfectCmsImages\Filesystem\PerfectCmsImageValidator;
+use Sunnysideup\PerfectCmsImages\Model\File\PerfectCmsImageDataExtension;
+
+class ImageManipulations extends Object
+{
+
+    private static $webp_enabled = true;
+
+    private static $webp_quality = 77;
+
+    private static $imageLinkCache = [];
+
+    /**
+     * work out the best image link.
+     *
+     * There are basically three options:
+     * a. if the height and/or width matches or is smaller than it should be
+     *    then just return natural image
+     * b. if crop is set to true then Fill
+     * c. otherwise resize Height/Width/Both
+     *
+     * @param  Image     $image
+     * @param  string    $name
+     * @param  bool|null $useRetina optional
+     * @param  bool|null $forMobile optional
+     * @param  int|null  $resizeToWidth optional
+     *
+     * @return string
+     */
+    public static function get_image_link($image, string $name, ?bool $useRetina = null, ?bool $forMobile = null, ?int $resizeToWidth = 0) : string
+    {
+        $cacheKey = $image->ClassName .'_'. $image->ID.'_'.$name.'_'.($useRetina ? 'Y':'N').'_'.($forMobile ? 'MY':'MN');
+        if(empty(self::$imageLinkCache[$cacheKey])) {
+            //work out perfect width and height
+            if ($useRetina === null) {
+                $useRetina = PerfectCMSImages::use_retina($name);
+            }
+            $crop = PerfectCMSImages::is_crop($name);
+
+            $multiplier = PerfectCMSImages::get_multiplier($useRetina);
+            $perfectWidth = PerfectCMSImages::get_width($name, true);
+            $perfectHeight = PerfectCMSImages::get_height($name, true);
+
+            if($forMobile){
+                $perfectWidth = PerfectCMSImages::get_mobile_width($name, true);
+                $perfectHeight = PerfectCMSImages::get_mobile_height($name, true);
+            }
+
+            $perfectWidth = $perfectWidth * $multiplier;
+            $perfectHeight = $perfectHeight * $multiplier;
+
+            //get current width and height
+            $myWidth = $image->getWidth();
+            $myHeight = $image->getHeight();
+
+            //if we are trying to resize to a width that is small than the perfect width
+            //and the resize width is small than the current width, then lets resize...
+            if(intval($resizeToWidth)) {
+                if($resizeToWidth < $perfectWidth && $resizeToWidth < $myWidth) {
+                    $perfectWidth = $resizeToWidth;
+                }
+            }
+            if ($perfectWidth && $perfectHeight) {
+
+                //if the height or the width are already perfect then we can not do anything about it.
+                if ($myWidth === $perfectWidth && $myHeight === $perfectHeight) {
+                    $link = $image->Link();
+                } elseif ($myWidth < $perfectWidth || $myHeight < $perfectHeight) {
+                    $link = $image->Pad(
+                        $perfectWidth,
+                        $perfectHeight,
+                        PerfectCMSImages::get_padding_bg_colour($name)
+                    )->Link();
+                } elseif ($crop) {
+                    $link = $image->Fill($perfectWidth, $perfectHeight)->Link();
+                } else {
+                    $link = $image->FitMax($perfectWidth, $perfectHeight)->Link();
+                }
+            } elseif ($perfectWidth) {
+                if ($myWidth === $perfectWidth) {
+                    $link = $image->Link();
+                }  elseif ($myWidth < $perfectWidth) {
+                    $link = $image->Link();
+                } elseif ($crop) {
+                    $link = $image->Fill($perfectWidth, $myHeight)->Link();
+                } else {
+                    $link = $image->ScaleWidth($perfectWidth)->Link();
+                }
+            } elseif ($perfectHeight) {
+                if ($myHeight === $perfectHeight) {
+                    $link = $image->Link();
+                } elseif ($myHeight < $perfectHeight) {
+                    $link = $image->Link();
+                } elseif ($crop) {
+                    $link = $image->Fill($myWidth, $perfectHeight)->Link();
+                } else {
+                    $link = $image->ScaleHeight($perfectHeight)->Link();
+                }
+            } elseif ($forMobile) {
+                $link = '';
+            } else {
+                $link = $image->Link();
+            }
+            self::$imageLinkCache[$cacheKey] = $link;
+        }
+
+        return self::$imageLinkCache[$cacheKey];
+    }
+
+    /**
+     * back-up image
+     * @param  string $name
+     * @param  Image $backupObject
+     * @param  string $backupField
+     *
+     * @return Image|null
+     */
+    public static function get_backup_image(string $name)
+    {
+        $image = null;
+        $backupObject = SiteConfig::current_site_config();
+        $backupField = $name;
+        if ($backupObject->hasMethod($backupField)) {
+            $image = $backupObject->$backupField();
+        }
+
+        return $image;
+    }
+
+    /**
+     * placeholder image
+     * @param  int    $perfectWidth
+     * @param  int    $perfectHeight
+     * @return string
+     */
+    public static function get_placeholder_image_tag(string $name) : string
+    {
+        $multiplier = PerfectCMSImages::get_multiplier(true);
+        $perfectWidth = PerfectCMSImages::get_width($name, true);
+        $perfectHeight = PerfectCMSImages::get_height($name, true);
+        $perfectWidth = $perfectWidth * $multiplier;
+        $perfectHeight = $perfectHeight * $multiplier;
+        if ($perfectWidth || $perfectHeight) {
+            if (!$perfectWidth) {
+                $perfectWidth = $perfectHeight;
+            }
+            if (!$perfectHeight) {
+                $perfectHeight = $perfectWidth;
+            }
+            $text = "$perfectWidth x $perfectHeight /2 = ".round($perfectWidth/2)." x ".round($perfectHeight/2)."";
+
+            return 'https://placehold.it/'.($perfectWidth).'x'.($perfectHeight).'?text='.urlencode($text);
+        } else {
+            return 'https://placehold.it/1500x1500?text='.urlencode('no size set');
+        }
+    }
+
+    public static function web_p_link(string $link) : string
+    {
+        if (self::web_p_enabled() && $link) {
+            $fileNameWithBaseFolder = Director::baseFolder() .$link;
+            $arrayOfLink = explode('.', $link);
+            $extension = array_pop($arrayOfLink);
+            $pathWithoutExtension = rtrim($link, '.' . $extension);
+            $webPFileName = $pathWithoutExtension . '_' . $extension . '.webp';
+            $webPFileNameWithBaseFolder = Director::baseFolder() . '/' .$webPFileName;
+            if(file_exists($fileNameWithBaseFolder)) {
+                if(isset($_GET['flush'])) {
+                    unlink($webPFileNameWithBaseFolder);
+                }
+                if(file_exists($webPFileNameWithBaseFolder)) {
+                    //todo: check that image is the same ...
+                } else {
+                    list($width, $height, $type, $attr) = getimagesize($fileNameWithBaseFolder);
+                    $img = null;
+                    switch ($type) {
+                      case 2:
+                          $img = imagecreatefromjpeg($fileNameWithBaseFolder);
+                          break;
+                      case 3:
+                          $img = imagecreatefrompng($fileNameWithBaseFolder);
+                          imagesavealpha($img, true); // save alphablending setting (important)
+                    }
+                    if($img) {
+                        $webp = imagewebp($img, $webPFileNameWithBaseFolder, Config::inst()->get('ImageManipulations', 'webp_quality'));
+                    }
+                }
+                return $webPFileName;
+            }
+        }
+
+        return $link;
+
+    }
+
+    public static function add_fake_parts($image, string $link) : string
+    {
+        if (class_exists('HashPathExtension')) {
+            if ($curr = Controller::curr()) {
+                if ($curr->hasMethod('HashPath')) {
+                    $link = $curr->HashPath($link, false);
+                }
+            }
+        }
+        if($image->Title) {
+            $imageClasses = Config::inst()->get('PerfectCMSImages', 'perfect_cms_images_append_title_to_image_links_classes');
+            if (in_array($image->ClassName, $imageClasses)) {
+                $link .= '?title=' . urlencode(Convert::raw2att($image->Title));
+            }
+        }
+
+        return $link;
+    }
+
+    public static function web_p_enabled() : bool
+    {
+        if( Config::inst()->get('ImageManipulations', 'webp_enabled')) {
+            if (function_exists('imagewebp')) {
+                if (function_exists('imagecreatefromjpeg')) {
+                    if (function_exists('imagecreatefrompng')) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+
+
+
+    }
+
+}
---
+++ src/Api/PerfectCMSImages.php
@@ -0,0 +1,388 @@
+<?php
+
+namespace Sunnysideup\PerfectCmsImages\Api;
+
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\Folder;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\ORM\FieldType\DBField;
+use SilverStripe\ORM\SS_List;
+use Sunnysideup\PerfectCmsImages\Filesystem\PerfectCmsImageValidator;
+use Sunnysideup\PerfectCmsImages\Model\File\PerfectCmsImageDataExtension;
+
+class PerfectCMSImages extends Object implements Flushable
+{
+
+
+    /**
+     *.htaccess content for assets ...
+     * @var string
+     */
+    private static $htaccess_content = <<<EOT
+<IfModule mod_rewrite.c>
+    RewriteEngine On
+    RewriteBase /
+
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteCond %{REQUEST_FILENAME} !-d
+    RewriteRule ^(.+)\.(v[A-Za-z0-9]+)\.(js|css|png|jpg|gif|svg|webp)$ $1.$3 [L]
+</IfModule>
+
+EOT;
+
+
+    /**
+     * force resample, put htaccess content if it does not exists.
+     */
+    public static function flush()
+    {
+        if (! Config::inst()->get('Image', 'force_resample')) {
+            Config::inst()->update('Image', 'force_resample', true);
+        }
+        if (class_exists('HashPathExtension')) {
+            if (ASSETS_PATH) {
+                if (! file_exists(ASSETS_PATH)) {
+                    Filesystem::makeFolder(ASSETS_PATH);
+                }
+                $fileName = ASSETS_PATH.'/.htaccess';
+                if (! file_exists($fileName)) {
+                    $string = Config::inst()->get('PerfectCMSImages', 'htaccess_content');
+                    file_put_contents($fileName, $string);
+                }
+            }
+        }
+    }
+
+
+    /**
+     * background image for padded images...
+     *
+     * @var string
+     */
+    private static $perfect_cms_images_background_padding_color = '#cccccc';
+
+     /**
+     * used to set the max width of the media value for mobile images,
+     * eg <source srcset="small.jpg, small2x.jpg 2x" media="(max-width: 600px)">
+     *
+     * @var string
+     */
+    private static $mobile_media_max_width = '600px';
+
+    /***
+     * details of the images
+     *     - width: 3200
+     *     - height: 3200
+     *     - folder: "myfolder"
+     *     - filetype: "try jpg"
+     *     - enforce_size: false
+     *     - folder: my-image-folder-a
+     *     - filetype: "jpg or a png with a transparant background"
+     *     - use_retina: true
+     *     - padding_bg_colour: '#dddddd'
+     *     - crop: true
+     *
+     * @var array
+     */
+    private static $perfect_cms_images_image_definitions = [];
+
+    /***
+     * Images Titles will be appended to the links only
+     * if the ClassName of the Image is in this array
+     * @var array
+     *
+     */
+    private static $perfect_cms_images_append_title_to_image_links_classes = [
+        Image::class,
+    ];
+
+    private static $retina_multiplier = 2;
+
+    public static function get_description_for_cms(string $name) : string
+    {
+        $widthRecommendation = PerfectCMSImages::get_width($name, false);
+        $heightRecommendation = PerfectCMSImages::get_height($name, false);
+        $useRetina = PerfectCMSImages::use_retina($name);
+        $recommendedFileType = PerfectCMSImages::get_file_type($name);
+        $multiplier = PerfectCMSImages::get_multiplier($useRetina);
+        if (!$recommendedFileType) {
+            $recommendedFileType = 'jpg';
+        }
+        if ($widthRecommendation) {
+            if (intval($widthRecommendation)) {
+                //cater for retina
+                $widthRecommendation = $widthRecommendation * $multiplier;
+                $actualWidthDescription = $widthRecommendation.'px';
+            } else {
+                $actualWidthDescription = $widthRecommendation;
+            }
+        } else {
+            $actualWidthDescription = 'flexible';
+        }
+        if ($heightRecommendation) {
+            if (intval($heightRecommendation)) {
+                //cater for retina
+                $heightRecommendation = $heightRecommendation * $multiplier;
+                $actualHeightDescription = $heightRecommendation.'px';
+            } else {
+                $actualHeightDescription = $heightRecommendation;
+            }
+        } else {
+            $actualHeightDescription = 'flexible';
+        }
+
+
+        $rightTitle = '';
+
+        if ($actualWidthDescription == 'flexible') {
+            $rightTitle .= 'Image width is flexible';
+        } else {
+            $rightTitle .= "Image should to be <strong>$actualWidthDescription</strong> wide";
+        }
+
+        $rightTitle .= ' and ';
+
+        if ($actualHeightDescription == 'flexible') {
+            $rightTitle .= 'height is flexible';
+        } else {
+            $rightTitle .= " <strong>$actualHeightDescription</strong> tall";
+        }
+
+        $rightTitle .= '<br />';
+        $maxSizeInKilobytes = PerfectCMSImages::max_size_in_kilobytes($name);
+        if ($maxSizeInKilobytes) {
+            $rightTitle .= 'Maximum file size: '.round($maxSizeInKilobytes / 1024, 2).' megabyte.';
+            $rightTitle .= '<br />';
+        }
+        if ($recommendedFileType) {
+            if (strlen($recommendedFileType) < 5) {
+                $rightTitle .= 'The recommend file type (file extension) is <strong>'.$recommendedFileType.'</strong>.';
+            } else {
+                $rightTitle .= '<strong>'.$recommendedFileType.'</strong>';
+            }
+        }
+
+        return $rightTitle;
+
+    }
+
+    /**
+     * @param string           $name
+     *
+     * @return boolean
+     */
+    protected static function image_info_available(string $name) : bool
+    {
+        $sizes = self::get_all_values_for_images();
+        //print_r($sizes);die();
+        return isset($sizes[$name]) ? true : false;
+    }
+
+
+    /**
+     * @param string           $name
+     *
+     * @return bool
+     */
+    public static function use_retina(string $name) : bool
+    {
+        return self::get_one_value_for_image($name, "use_retina", true);
+    }
+
+    /**
+     * @param bool           $useRetina
+     *
+     * @return int
+     */
+    public static function get_multiplier(bool $useRetina) : int
+    {
+        $multiplier = 1;
+        if ($useRetina) {
+            $multiplier = Config::inst()->get('PerfectCMSImages', 'retina_multiplier');
+        }
+        if(! $multiplier) {
+            $multiplier = 1;
+        }
+        return $multiplier;
+
+    }
+
+
+    /**
+     * @param string           $name
+     *
+     * @return boolean
+     */
+    public static function is_crop(string $name) : bool
+    {
+        return self::get_one_value_for_image($name, "crop", false);
+    }
+
+    /**
+     * @param string           $name
+     * @param bool             $forceInteger
+     *
+     * @return int?string
+     */
+    public static function get_width(string $name, bool $forceInteger = false)
+    {
+        $v = self::get_one_value_for_image($name, "width", 0);
+        if ($forceInteger) {
+            $v = intval($v) - 0;
+        }
+
+        return $v;
+    }
+
+    /**
+     * @param string           $name
+     * @param bool             $forceInteger
+     *
+     * @return int|string
+     */
+    public static function get_height(string $name, bool $forceInteger = false)
+    {
+        $v = self::get_one_value_for_image($name, "height", 0);
+        if ($forceInteger) {
+            $v = intval($v) - 0;
+        }
+
+        return $v;
+    }
+
+    /**
+     * @param string           $name
+     * @param bool             $forceInteger
+     *
+     * @return int?string
+     */
+    public static function get_mobile_width(string $name, bool $forceInteger = false)
+    {
+        $v = self::get_one_value_for_image($name, "mobile_width", 0);
+        if ($forceInteger) {
+            $v = intval($v) - 0;
+        }
+
+        return $v;
+    }
+
+    /**
+     * @param string           $name
+     * @param bool             $forceInteger
+     *
+     * @return int|string
+     */
+    public static function get_mobile_height(string $name, bool $forceInteger = false)
+    {
+        $v = self::get_one_value_for_image($name, "mobile_height", 0);
+        if ($forceInteger) {
+            $v = intval($v) - 0;
+        }
+
+        return $v;
+    }
+
+    /**
+     * @param string           $name
+     *
+     * @return string
+     */
+    public static function get_folder(string $name) : string
+    {
+        return self::get_one_value_for_image($name, "folder", 'other-images');
+    }
+
+    /**
+     * @param string           $name
+     *
+     * @return int
+     */
+    public static function max_size_in_kilobytes(string $name) : int
+    {
+        $maxSizeInKilobytes = self::get_one_value_for_image($name, "max_size_in_kilobytes", 0);;
+        if (! $maxSizeInKilobytes) {
+            $maxSizeInKilobytes = Config::inst()->get('PerfectCMSImagesUploadField', 'max_size_in_kilobytes');
+        }
+        return intval($maxSizeInKilobytes) - 0;
+    }
+
+    /**
+     * @param string           $name
+     *
+     * @return string
+     */
+    public static function get_file_type(string $name) : string
+    {
+        return self::get_one_value_for_image($name, "filetype", 'jpg');
+    }
+
+    /**
+     * @param string           $name
+     *
+     * @return boolean
+     */
+    public static function get_enforce_size(string $name) :bool
+    {
+        return self::get_one_value_for_image($name, "enforce_size", false);
+    }
+
+    /**
+     * @param string           $name
+     *
+     * @return string
+     */
+    public static function get_mobile_media_width(string $name) : string
+    {
+        return self::get_one_value_for_image(
+            $name,
+            "mobile_media_max_width",
+            Config::inst()->get('PerfectCMSImages', 'mobile_media_max_width')
+        );
+    }
+
+    /**
+     * @param string           $name
+     *
+     * @return string
+     */
+    public static function get_padding_bg_colour(string $name) : string
+    {
+        return self::get_one_value_for_image(
+            $name,
+            "padding_bg_colour",
+            Config::inst()->get('PerfectCMSImages', 'perfect_cms_images_background_padding_color')
+        );
+    }
+
+    /**
+     * @param string $name
+     * @param int    $key
+     * @param mixed  $default
+     *
+     * @return mixed
+     */
+    protected static function get_one_value_for_image(string $name, string $key, ?string $default = '')
+    {
+        $sizes = self::get_all_values_for_images();
+        //print_r($sizes);die();
+        if (isset($sizes[$name])) {
+            if (isset($sizes[$name][$key])) {
+                return $sizes[$name][$key];
+            }
+        } else {
+            user_error('no information for image with name: '.$name);
+        }
+
+        return $default;
+    }
+
+    /**
+     * @return array
+     */
+    protected static function get_all_values_for_images() : array
+    {
+        return Config::inst()->get('PerfectCMSImages', 'perfect_cms_images_image_definitions');
+    }
+
+
+}
